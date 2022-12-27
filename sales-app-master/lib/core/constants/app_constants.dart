06f60462742092bccf1c5773903bd34aebe20c6f
ismayil-allahverdiyev <allahverdiyev.ismayl@mail.ru>
2022-12-27 22:43:35 +0300

    Uploading to the server

--- sales-app-master/lib/core/constants/app_constants.dart
+++ sales-app-master/lib/core/constants/app_constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-String uri = "http://192.168.130.26:3000";
+String uri = "http://192.168.36.26:3000";
 
 class AppConstants {
   static const Color primaryColor = Color(0xff762ec1);
